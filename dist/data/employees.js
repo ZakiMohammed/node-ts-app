27c8547b66eb66e672889d94955b41a534ae912e
Zaki Shaikh <zaki-shaikh@outlook.com>
2022-05-18 06:25:18 +0530

    first commit

---
+++ dist/data/employees.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const employees = [
+    { id: '1', name: 'Bob', job: 'Clerk', department: 'Accounts', salary: 1200 }
+];
